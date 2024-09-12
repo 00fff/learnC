adc56fb14bd07447ba65a4f69b923d63ceb69490
tanite <harithsullaiman@gmail.com>
2024-09-12 12:05:28 -0400

    Working on 1.6 Problem

---
+++ Chaper1/LearnC1-6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/* count digits, white space, others */
+int main()
+{
+    // int c, i, nwhite, nother;
+    // int ndigit[10];
+    
+    // nwhite = nother = 0;
+    // for (i = 0; i < 10; ++i)
+    //     ndigit[i] = 0;
+    
+    // while ((c = getchar()) != EOF)
+    //     if (c >= '0' && c <= '9')
+    //         ++ndigit[c - '0'];
+    //     else if (c == ' ' || c == '\n' || c == '\t')
+    //         ++nwhite;
+    //     else
+    //         ++nother;
+    
+    // printf("digits =");
+    // for (i = 0; i < 10; ++i)
+    //     printf(" %d", ndigit[i]);
+    // printf(", white space = %d, other = %d\n", nwhite, nother);
+    // Challenge 1
+    int c, i, length = {0};
+    int wordsOne[10] = {0}; // Initialize arrays to zero
+    int wordsTwo[10] = {0}; // Initialize arrays to zero
+
+    while ((c = getchar()) != EOF) {
+        length++;
+        for (length; length <= length++; ++i) 
+            wordsOne[i] = i + 1;
+            printf("%d ", wordsOne[i]);
+            
+        
+             
+
+    }
+}
