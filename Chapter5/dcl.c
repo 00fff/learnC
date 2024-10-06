4cd2fbb6a56d6249cfac7a4f3e1a0ce0c7ec34ed
tanite <harithsullaiman@gmail.com>
2024-10-06 16:15:56 -0400

    the 6th

---
+++ Chapter5/dcl.c
@@ -0,0 +1,28 @@
+void dcl(void) {
+    int ns;
+    for (ns = 0; gettoken() == '*'; ) /* count *'s */
+        ns++;
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, " pointer to");
+}
+
+void dirdcl(void) {
+    int type;
+    if (tokentype == '(') { /* ( dcl ) */
+        dcl();
+        if (tokentype != ')')
+            printf("error: missing )\n");
+    } else if (tokentype == NAME) /* variable name */
+        strcpy(name, token);
+    else
+        printf("error: expected name or (dcl)\n");
+    while ((type=gettoken()) == PARENS || type == BRACKETS)
+        if (type == PARENS)
+            strcat(out, " function returning");x
+        else {
+            strcat(out, " array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
+}
